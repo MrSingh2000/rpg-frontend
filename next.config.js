d6a0537aa3a8cdc8218a83ee9f87f6d7f6c52232
Anshuman Singh <singhanshuman1020@gmail.com>
2022-11-19 01:20:56 +0530

    disable eslint error during build

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['https:/unsplash.com/']
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
